7c3520400d15d297cd157951d45f801853095b8c
Thirukandiyur Sudarshan Karthk <thirusudarshan@gmail.com>
2024-03-13 23:19:06 +0530

    structure code

--- Basics.py
+++ Basics.py
@@ -2,22 +2,27 @@ import cv2
 import numpy as np
 import face_recognition
 
-imageChris = face_recognition.load_image_file('./Base/chris-nolan.jpg')
-imageChris = cv2.cvtColor(imageChris, cv2.COLOR_BGR2RGB)
 
-imageTest = face_recognition.load_image_file('./Base/christopher-nolan.jpg')
-imageTest = cv2.cvtColor(imageTest, cv2.COLOR_BGR2RGB)
+def loadImage(url):
+    image = face_recognition.load_image_file(url)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-faceLocChris = face_recognition.face_locations(imageChris)[0]
-encodeChris = face_recognition.face_encodings(imageChris)[0]
+    location = face_recognition.face_locations(image)[0]
+    encoding = face_recognition.face_encodings(image)[0]
 
-cv2.rectangle(imageChris, (faceLocChris[3], faceLocChris[0]), (faceLocChris[1], faceLocChris[2]), (0, 255, 0), 2)
-cv2.imshow('ChrisNolan', imageChris)
-cv2.waitKey(0)
+    return image, location, encoding
 
-faceLocTest = face_recognition.face_locations(imageTest)[0]
-encodeTest = face_recognition.face_encodings(imageTest)[0]
 
-cv2.rectangle(imageTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (0, 255, 0), 2)
-cv2.imshow('ChrisNolan', imageTest)
-cv2.waitKey(0)
+def drawImage(image, location, imageName="image"):
+    if location:
+        cv2.rectangle(image, (location[3], location[0]), (location[1], location[2]), (0, 255, 0), 2)
+    cv2.imshow(imageName, image)
+    cv2.waitKey(0)
+
+
+imageChris, locChris, encodeChris = loadImage('./Base/chris-nolan.jpg')
+imageTest, locTest, encodeTest = loadImage('./Base/christopher-nolan.jpg')
+
+drawImage(imageChris, locChris, "Chris Image")
+drawImage(imageTest, locTest, "Test Image")
+
